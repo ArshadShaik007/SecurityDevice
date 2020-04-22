7926514822acd4aefc4fef8ad1286d8ac50ad40c
Arshad <arshad007.vip@gmail.com>
2020-04-22 19:09:28 +0530

    DONE WITH THE PYTHON FLASK SERVER

--- application.py
+++ application.py
@@ -5,3 +5,7 @@ app=Flask("__name__")
 @app.route("/")
 def index():
     return "HEllo,World"
+@app.route("/<string:name>")
+def arshad(name):
+    name=name.capitalize()
+    return f"HELLO FEARMAN {name}"
